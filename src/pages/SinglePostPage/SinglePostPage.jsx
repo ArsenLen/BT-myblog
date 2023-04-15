6bdfceca5314101f3564c28aac9fdf520567360e
ArsenLen <laurie10@mail.ru>
2023-04-15 20:00:25 +0600

    register page added

--- src/pages/SinglePostPage/SinglePostPage.jsx
+++ src/pages/SinglePostPage/SinglePostPage.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './singlepostpage.module.css';
 import postImg from './postimg.png';
 import { useParams } from 'react-router-dom';
 import postService from '../../services/posts';
 
 const SinglePostPage = () => {
+  const [post, setPost] = useState({})
   const { id } = useParams() // useParams() - { id: r3ty34ur46 }
+  
+  useEffect(() => {
+    postService
+      .getPost(id)
+      .then(res => setPost(res.data))
+  }, [])
     return (
       <article className={styles.post}>
-        <h1 className={styles.title}>My title</h1>
-        <img src={postImg} alt="postImg" className={styles.img} />
+        <h1 className={styles.title}>{post.title}</h1>
+        <img src={post.img} alt="postImg" className={styles.img} />
         <p className={styles.descr}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam enim
-          aspernatur cupiditate veritatis vero, porro, aliquam est quos laborum
-          voluptates, deleniti repellat eos nobis sed eius facere ipsa nulla vel
-          odit fugiat maiores harum numquam illo repellendus. Dignissimos
-          laborum ipsam voluptate perspiciatis quisquam veniam quos provident
-          esse quo consequatur? Error natus corrupti ad in ab iure fugit hic
-          minima tempora culpa aliquid, nulla suscipit quaerat consequuntur unde
-          assumenda accusantium minus!
+          {post.descr}
         </p>
-        <p className={styles.date}>Published 11 April 2023</p>
+        <p className={styles.date}>{post.createdAt}</p>
       </article>
     );
 };
