071c27e40d499eb72c1fa5701164501c8422a2d4
ArsenLen <laurie10@mail.ru>
2023-04-20 20:01:23 +0600

    login page

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import styles from '../RegisterPage/registerpage.module.css';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const login = (loginData) => {
+    return axios.post("https://gentle-ridge-36337.herokuapp.com/api/auth/login", loginData)
+}
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const userObj = {
+            email,
+            password
+        }
+        try {
+            const res = await login(userObj) 
+            toast("Вы успешно авторизовались")
+        } catch (error) {
+            toast(error.response.data)
+        }
+    }
+    return (
+        <section className={styles.wrapper}>
+            <h1 className={styles.title}>Логин</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
+            <TextField 
+                    label="Ваш email" 
+                    variant="filled" 
+                    type="email"
+                    name="email"
+                    fullWidth
+                    required
+                    style={{ marginBottom: "20px" }}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <TextField 
+                    label="Ваш пароль" 
+                    variant="filled" 
+                    type="password"
+                    name="password"
+                    fullWidth
+                    required
+                    style={{ marginBottom: "40px" }}
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button 
+                    fullWidth 
+                    type="submit" 
+                    variant="contained"
+                    style={{backgroundColor: "#BF94E8", fontSize: "24px"}}
+                    >
+                    Логин
+                </Button>
+            </form>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
+        </section>
+    );
+};
+
+export default LoginPage;
